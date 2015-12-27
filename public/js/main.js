a0f86e56552865f3337dd99b44b8574d71a409ae
anlauren <antoine.lauren.f@gmail.com>
2015-12-27 14:42:58 +0100

    list synchronized with array + remove button in list
    bug in removal to be fixed

--- public/js/main.js
+++ public/js/main.js
@@ -3,16 +3,16 @@
 /* ################################################ */
 
 /* Objects definitions */
-function cryptoDevice(hardwareSlot, manufacturerID, removableDevice, slotDescription, tokenPresent)
+function cryptoDevice(id, hardwareSlot, manufacturerID, removableDevice, slotDescription, tokenPresent)
 {
+	this.id=id;
 	this.hardwareSlot = hardwareSlot;
 	this.manufacturerID = manufacturerID;
 	this.removableDevice = removableDevice;
 	this.slotDescription = slotDescription;
 	this.tokenPresent = tokenPresent;
-
-	this.display = function display(){ //return text pannel with fields of the device.
-		var res ="<div class='col s3 card-panel'><table class='striped'><thead><tr><th data-field='id'>Name</th><th data-field='name'>Value</th></tr></thead><tbody>";
+	this.display = function display(){ //return text pannel with fields of the device. the id of this device is "cd-*its number*"
+		var res ="<div class='col s3 card-panel' id='cd-"+this.id+"'><table class='striped'><thead><tr><th data-field='id'>Name</th><th data-field='name'>Value</th></tr></thead><tbody>";
 		for(e in this)
 		{
 			if(e != "display")
@@ -30,11 +30,16 @@ function cryptoDevice(hardwareSlot, manufacturerID, removableDevice, slotDescrip
 /* Global Variables */
 
 var cdList = new Array();
+function findIndexById(id)
+{
 
-/* graphic environment set up       */
-
-	$(".button-collapse").sideNav();
-	$('.modal-trigger').leanModal();
+		for(var i=0;i<cdList.length;i++)
+		{
+			if(cdList[i].id == id)
+				return i;
+		}
+		return -1;
+}
 
 
 /* pks11 api interface  request     */
@@ -57,17 +62,24 @@ function addCd(JsonAnswer)//adds a CD from Json answer into the crypto-device li
 	//push the object
 	cdList.push(cD);
 	// actualize collapsed list
-		//to implement
+	actualizeList();
 	// display in central pannel
 	displayCd(cD);
 }
-function removeCd(position)//removes a CD from the cryptoDevice list in the position position (integer Required)
+function removeCd(id)//removes a CD from the cryptoDevice list in the position position (integer Required)
 {
+	console.log("id received:");
+	console.log(id);
+	var position = findIndexById(id);
+	console.log("index received:");
+	console.log(position);
 	// we can think to pup up a confirmation panel here
+
 	cdList.splice(position, 1);
 	//if displayed, remove
-		//to implement
+	unDisplayCd(id);	
 	//actualize display of collapsed list
+	actualizeList();
 }
 function displayCd(cD)//display a CD selected int the central panel
 {
@@ -76,13 +88,67 @@ function displayCd(cD)//display a CD selected int the central panel
 	$('#display-panel').html(tmp + cD.display());
 	//to implement
 }
-function testDisp()// test function that display a random cD into the central panel
+function unDisplayCd(id)//remove CD selected int the central panel
 {
+	
+	console.log("id :");
+	console.log(id);
+	$("#cd-"+id).remove();
+
+}
+function actualizeList()
+{
+	var res="";
+	$("#crypto-list").html("");
+	console.log("cd list :");
+	console.log(cdList);
+	if (cdList.length !=0)
+	{
+		for(var e=0; e<cdList.length; e++)
+		{
+			console.log("element :");
+			console.log(cdList[e]);
+			console.log("param rec:");
+			console.log(cdList[e].id);
+			if(e !='undefined')
+			{
+				res +="<li class='collection-item' id='cList-"+cdList[e].id+"'>crypto-device number : "+cdList[e].id;
+				res +="<a href='#' class='secondary-content'><i class='material-icons' id='rm-cd-"+cdList[e].id+"'>close</i></a></li>";
+				$("#crypto-list").html($("#crypto-list").html()+res);
+				$("#rm-cd-"+cdList[e].id).click((function _clicRm(index){
+																		return function _removeCd()
+																									{
+																										removeCd(cdList[index].id);
+																									}
+																		})(e)	
+												);							
+																	
+				res="";
+			}
+		}
+	}
+		
+}
+function testDisp(nb)// test function that display a random cD into the central panel
+{
+	 if (typeof(nb)==='undefined') nb = 1;
 	var cD = new cryptoDevice();
+	cD.id=nb;
 	cD.hardwareSlot = "yes";
 	cD.manufacturerID = "yes";
 	cD.removableDevice = "yes"
 	cD.slotDescription  = "yes";
 	cD.tokenPresent = "yes";
+	cdList.push(cD);
 	displayCd(cD);
+	actualizeList();
 }
+
+/* graphic environment set up       */
+
+	$(".button-collapse").sideNav();
+	$('.modal-trigger').leanModal();
+	testDisp(1);
+	testDisp(2);
+	testDisp(3);
+	actualizeList();
