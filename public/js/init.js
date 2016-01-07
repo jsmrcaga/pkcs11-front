cc7d7f081649b9d3ebde1df1d5cafebbf9ac8c23
anlauren <antoine.lauren.f@gmail.com>
2016-01-07 01:35:25 +0100

    add afreakingline

--- public/js/init.js
+++ public/js/init.js
@@ -303,8 +303,9 @@ document.getElementById("button_token_random").addEventListener("click", functio
 			}
 			Materialize.toast("Random generated!", 3000, "toast-success");
 			$("#modal_token_random_wait").closeModal();
-			$("#random-data").html(" Data generated : "+res);
-			$("#random-data").openModal();
+			var rand = JSON.parse(res);
+			$("#random-data").html(" Data generated : "+rand.bytesArray);
+			$("#modal_random_display").openModal();
 		});
 	};	
 	$("#input-wraper").display="none";					
